75242cef830e6abe0eccb32a015c9127b230916a
ynagaraj <yeshwanth.nagaraj@aptean.com>
2018-09-15 11:56:56 +0530

    Added approach to Optimum House coloring problem

--- OptimizationProblems/OptimumHouseColoring/src/optimumhousecoloring/OptimumHouseColoring.java
+++ OptimizationProblems/OptimumHouseColoring/src/optimumhousecoloring/OptimumHouseColoring.java
@@ -3,7 +3,34 @@
 He has a goal of minimizing cost while ensuring that no two neighboring houses are of the same color.
 Given an N by K matrix where the nth row and kth column represents the cost to build the nthhouse with kth color, 
 return the minimum cost which achieves this goal.
+======================================================================================================================
+Approach:
+Dynamic Programming solution:
+we can paint the ith house with blue, red or green.
+so we have the following equations:
+cost[i,r] = min( cost[i-1,b], cost[i-1,g] ) + cost of painting i with r.
+cost[i,g] = min( cost[i-1,b], cost[i-1,r] ) + cost of painting i with g.
+cost[i,b] = min( cost[i-1,r], cost[i-1,g] ) + cost of painting i with b.
 
+
+Final Min Cost = min (cost[n,b], cost[n,r], cost[n,g]);
+
+
+---------------------------
+    R      G       B
+---------------------------
+1   7      5       10 
+---------------------------
+2   3      6        1
+---------------------------
+3   8      7       4
+--------------------------
+4   6      2       9
+--------------------------
+5   1      4       7
+--------------------------
+6   2      3       6
+--------------------------
  */
 package optimumhousecoloring;
 
